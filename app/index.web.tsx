1a3abc19f7a371066dc9263f4c36e3b73a5f24f9
maruf <marufgithub@gmail.com>
2025-09-10 11:33:07 +0600

    version 3 and perfectly according to official docs

--- app/index.web.tsx
+++ app/index.web.tsx
@@ -1,12 +1,12 @@
 import { WithSkiaWeb } from "@shopify/react-native-skia/lib/module/web";
+import { version } from 'canvaskit-wasm/package.json';
 import { Text } from "react-native";
 
-export default function Index() {
+export default function App() {
   return (
     <WithSkiaWeb
-      opts={{
-        locateFile: (file: string) =>
-          `https://cdn.jsdelivr.net/npm/canvaskit-wasm@0.40/bin/full/${file}`,
+      opts={{ 
+        locateFile: (file) => `https://cdn.jsdelivr.net/npm/canvaskit-wasm@${version}/bin/full/${file}` 
       }}
       getComponent={() => import("../Breathe")}
       fallback={<Text style={{ textAlign: "center" }}>Loading Skia...</Text>}
